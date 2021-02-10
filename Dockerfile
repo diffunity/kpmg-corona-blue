c6e4cc2f257b22f5bec0021bf7e87e02c1152ef9
eppen <diffunity@gmail.com>
2021-02-10 11:59:15 +0900

    !pushed for deployment!

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+FROM python:3.8.3
 
 COPY . /worker
 WORKDIR /worker
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # unittest
-echo "begin unittest"
+echo "Begin unittest"
 python3 ./src/test.py
 
 # deployment
-echo -e "unittest finished\nbegin deployment"
+echo -e "Unittest finished"
+echo "========================================================="
+echo "Begin deployment"
 
 # export requirements
 pip install --upgrade pip
@@ -13,6 +15,7 @@ pipreqs . --print >> ./conf/requirements.txt
 
 # export python version
 pyver=$(python --version)
+echo -e "Python version ${pyver} detected"
 img="FROM python:${pyver:7:${#pyver}}"
 
 # set dockerfile
@@ -29,21 +32,37 @@ then
   fi
 fi
 
+echo "========================================================="
+echo "Pushing to git"
+git add .
+git commit -m "!pushed for deployment!"
+git push
+echo "!!!pushed to git!!!"
+echo "   Commit message: pushed for deployment"
+echo "========================================================="
+
+echo "Building Dockerfile"
+echo "** Note: If you are deploying this on your local computer, you may ignore the messages below"
+echo "**       The below process is for the purposes of actual deployment"
+
 # build and run dockerfile
 if [ "${1}" == "" ]
 then
-  echo "please give the image name as the first argument"
-  echo "exiting due to no given image name"
+  echo "ERROR: please give the image name as the first argument"
+  echo "! exiting due to no given image name !"
   exit 1
 else
   echo "building image ${1}"
   sudo docker build -t "${1}" .
   if [ "${2}" == "" ]
   then
-    echo -e "container name was not given.\nto give container name, pass it as the second argument" 
+    echo -e "NOTE: container name was not given.\nto give container name, pass it as the second argument" 
     sudo docker run "${1}"
   else
-    echo "containerizing image ${1} as ${2}"
+    echo "! containerizing image ${1} as ${2} !"
     sudo docker run --name "${2}" "${1}"
   fi
 fi
+
+echo "Successfully Deployed"
+echo "========================================================="
