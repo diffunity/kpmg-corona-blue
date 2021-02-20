d41f8ddd872ff3a752ecc8c29ffdc9e47e69a45f
Sieun <tldms0327@yonsei.ac.kr>
2021-02-20 15:35:36 +0900

    add: postAPI emotion-check

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,12 @@ from app.dependencies.make_request import MakeRequest
 from app.schema import *
 
 setup_logger()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s : %(message)s',
+                    datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger(__name__)
 
-
 app = FastAPI()
+make_request = MakeRequest()
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
@@ -21,3 +23,16 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], all
 @app.get("/health")
 def health_check():
     return {"ok": True}
+
+
+@app.post("/emotion-check", response_model=JobRequestResponse)
+def emotion_check(
+        request_body: JobRequestBody
+):
+    logger.info(f"Request info: {request_body} ")
+
+    result = make_request.make_job(request_body.project_type,
+                                   request_body.request_time,
+                                   request_body.data)
+
+    return JobRequestResponse(request_ids=result)
