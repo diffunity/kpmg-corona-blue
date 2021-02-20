b7a259714a69e80d82a426cafd98d6debe04d926
Sieun <tldms0327@yonsei.ac.kr>
2021-02-20 16:23:43 +0900

    chore: change directory name

---
+++ model/src/model.py
@@ -0,0 +1,73 @@
+import json
+import yaml
+import logging
+
+import torch
+import numpy as np
+import tensorflow as tf
+
+logger = logging.getLogger(__name__)
+
+class model:
+    def __init__(self):
+        
+        ###########
+        # model initialization (modeller)
+        # 
+        # 
+        # 
+        # 
+        ###########
+
+        ########### example
+        self.config = yaml.load(open("./conf/config.yml", "r"), Loader=yaml.SafeLoader)
+        self.model = torch.load(self.config["model_settings"]["pretrained_filepath"])
+        self.model.eval()
+        ###########
+
+    def inference(self, message:json):
+        ###########
+        # jsonify model results (modeller)
+        # 
+        # 
+        # 
+        # 
+        ###########
+
+        ########### example
+        frame = message["input"]
+        output = self.model(torch.Tensor(frame))
+        return {"output": output.detach().numpy().tolist()}
+        ###########
+
+    def deploy(self):
+
+        ###########
+        # sqs queue initialization (backend)
+        ###########
+        
+        look_at_queue = {"input": np.ndarray(self.config["unittest_settings"]["input_dimension"])}
+        output = self.inference(look_at_queue)
+
+        ###########
+        # send back result to API by sqs (backend)
+        # 
+        ###########
+
+    def run(self):
+        print("Model successfully deployed")
+        while True:
+            self.deploy()
+
+if __name__=="__main__":
+    
+    ###########
+    # sqs queue initialization (backend)
+    # 
+    # 
+    # 
+    # 
+    ###########
+    model = model()
+
+    model.run()
---
+++ model/src/sqs.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+class SqsController:
+    def __init__(self):
+
+
+
+
+
