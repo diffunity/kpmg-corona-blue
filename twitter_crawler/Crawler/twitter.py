52aa19036099f2eba64ef9ad23474e88a5373ad1
Sieun <tldms0327@yonsei.ac.kr>
2021-02-21 21:22:47 +0900

    add: real time twitter analysis

--- twitter_crawler/Crawler/twitter.py
+++ twitter_crawler/Crawler/twitter.py
@@ -1,6 +1,8 @@
 import tweepy
 import re
 import json
+import requests
+import time
 from collections import OrderedDict
 
 from twitter_config import TWITTER_CONFIG
@@ -40,126 +42,32 @@ def get_posts(account):
 
         jsonfile[post_id] = {"time": time, "text": text, "image": image}
 
-    output = json.dumps(jsonfile, ensure_ascii=False, indent="\t")
+    output = jsonfile
     return output
 
 
 if __name__ == '__main__':
-    print(get_posts("@gjwlsdnr0115"))
+    posts_result = get_posts("@gjwlsdnr0115")
+    posts = dict(posts_result)
 
-# from __future__ import print_function
-# import getopt
-# import logging
-# import os
-# import sys
-# # import traceback
-# # third-party: `pip install tweepy`
-# import tweepy
+    request_url = "http://ec2co-ecsel-f2k8u3ar7ixb-1602496836.ap-northeast-2.elb.amazonaws.com:8000/emotion-check/"
+    for key in posts:
+        request_body = dict()
+        request_body["project_type"] = "twitter"
+        request_body["data"] = {"type": "text",
+                                "create_date_time": str(posts[key]["time"]),
+                                "content": str(posts[key]["text"])}
 
-# # global logger level is configured in main()
-# Logger = None
+        r = requests.post(request_url + 'text', data=json.dumps(request_body))
+        print(r.json())
 
-# # Generate your own at https://apps.twitter.com/app
-# CONSUMER_KEY = "5QBfZ8fanPj4MKRZF033Aw1YL"
-# CONSUMER_SECRET = "lR7BesIo0o6nqv1xF2JaBG4DjNbb1DzNCftDNLM5MhARAQcHJT"  '
-# OAUTH_TOKEN = "1325976318962401280-yCyQ0vZtf0jU6en9ujGpD5yXqtL7DO"
-# OAUTH_TOKEN_SECRET = "D4y80Cfmhbi8WB1tTBYqrseSZtBSL31Q6Vzi6Q9MnJBOE"
+        if len(posts[key]["image"]) > 0:
+            request_body = dict()
+            request_body["project_type"] = "twitter"
+            request_body["data"] = {"type": "photo",
+                                    "create_date_time": posts[key]["time"],
+                                    "content": posts[key]["image"]}
+            r = requests.post(request_url + 'image', data=json.dumps(request_body))
+            print(r.json())
 
-# # batch size depends on Twitter limit, 100 at this time
-# batch_size=100
-
-# def get_tweet_id(line):
-#     '''
-#     Extracts and returns tweet ID from a line in the input.
-#     '''
-#     (tagid,_timestamp,_sandyflag) = line.split('\t')
-#     (_tag, _search, tweet_id) = tagid.split(':')
-#     return tweet_id
-
-# def get_tweets_single(twapi, idfilepath):
-
-#     # process IDs from the file
-#     with open(idfilepath, 'rb') as idfile:
-#         for line in idfile:
-#             tweet_id = get_tweet_id(line)
-#             Logger.debug('get_tweets_single: fetching tweet for ID %s', tweet_id)
-#             try:
-#                 tweet = twapi.get_status(tweet_id)
-#                 print('%s,%s' % (tweet_id, tweet.text.encode('UTF-8')))
-#             except tweepy.TweepError as te:
-#                 Logger.warn('get_tweets_single: failed to get tweet ID %s: %s', tweet_id, te.message)
-#                 # traceback.print_exc(file=sys.stderr)
-#         # for
-#     # with
-
-# def get_tweet_list(twapi, idlist):
-
-#     # fetch as little metadata as possible
-#     tweets = twapi.statuses_lookup(id_=idlist, include_entities=False, trim_user=True)
-#     if len(idlist) != len(tweets):
-#         Logger.warn('get_tweet_list: unexpected response size %d, expected %d', len(tweets), len(idlist))
-#     for tweet in tweets:
-#         print('%s,%s' % (tweet.id, tweet.text.encode('UTF-8')))
-
-# def get_tweets_bulk(twapi, idfilepath):
-
-#     # process IDs from the file
-#     tweet_ids = list()
-#     with open(idfilepath, 'rb') as idfile:
-#         for line in idfile:
-#             tweet_id = get_tweet_id(line)
-#             Logger.debug('Enqueing tweet ID %s', tweet_id)
-#             tweet_ids.append(tweet_id)
-#             # API limits batch size
-#             if len(tweet_ids) == batch_size:
-#                 Logger.debug('get_tweets_bulk: fetching batch of size %d', batch_size)
-#                 get_tweet_list(twapi, tweet_ids)
-#                 tweet_ids = list()
-#     # process remainder
-#     if len(tweet_ids) > 0:
-#         Logger.debug('get_tweets_bulk: fetching last batch of size %d', len(tweet_ids))
-#         get_tweet_list(twapi, tweet_ids)
-
-# def usage():
-#     print('Usage: get_tweets_by_id.py [options] file')
-#     print('    -s (single) makes one HTTPS request per tweet ID')
-#     print('    -v (verbose) enables detailed logging')
-#     sys.exit()
-
-# def main(args):
-#     logging.basicConfig(level=logging.WARN)
-#     global Logger
-#     Logger = logging.getLogger('get_tweets_by_id')
-#     bulk = True
-#     try:
-#         opts, args = getopt.getopt(args, 'sv')
-#     except getopt.GetoptError:
-#         usage()
-#     for opt, _optarg in opts:
-#         if opt in ('-s'):
-#             bulk = False
-#         elif opt in ('-v'):
-#             Logger.setLevel(logging.DEBUG)
-#             Logger.debug("main: verbose mode on")
-#         else:
-#             usage()
-#     if len(args) != 1:
-#         usage()
-#     idfile = args[0]
-#     if not os.path.isfile(idfile):
-#         print('Not found or not a file: %s' % idfile, file=sys.stderr)
-#         usage()
-
-#     # connect to twitter
-#     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-#     auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-#     api = tweepy.API(auth)
-
-#     # hydrate tweet IDs
-#     if bulk:
-#         get_tweets_bulk(api, idfile)
-#     else:
-#         get_tweets_single(api, idfile)
-
-# if __name__ == '__main__':
-#     main(sys.argv[1:])
+        time.sleep(2)
--- twitter_crawler/Crawler/twitter_config.py
+++ twitter_crawler/Crawler/twitter_config.py
@@ -2,5 +2,5 @@ import boto3
 import json
 
 client = boto3.client('s3')
-obj = client.get_object(Bucket='kpmg-ybigta', Key='config/twitter_config.json')
+obj = client.get_object(Bucket='kpmgybigta', Key='config/twitter_config.json')
 TWITTER_CONFIG = json.loads(obj['Body'].read())
