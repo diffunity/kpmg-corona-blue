f1e08f880bd141b2c91f63556649a67f66ac089c
eppen <diffunity@gmail.com>
2021-02-21 20:12:36 +0900

    API for update function

--- backend/main.py
+++ backend/main.py
@@ -128,3 +128,8 @@ def get_result(request_body: StatusCheckRequest):
     return json.loads({"text_analysis": text_result, "tone_analysis": audio_result})
 
 
+# update
+@app.get("/update/emotion-result/text")
+def get_update_result(request_body: UpdateREquest):
+    assert UpdateRequest==1, "Sure, sure!"
+    result = analysis.
