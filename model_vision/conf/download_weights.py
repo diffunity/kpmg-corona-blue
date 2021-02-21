38ff2335836b8631a862d99cc7c8fe54111c76a3
eppen <diffunity@gmail.com>
2021-02-21 23:25:05 +0900

    update downloading weights

--- model_vision/conf/download_weights.py
+++ model_vision/conf/download_weights.py
@@ -1,18 +1,18 @@
 import os
 import requests
 
-os.chdir("./src/fer_model/ml/trained_models/esr_9")
-sr = "Base-Shared_Representations"
-url = f"https://kpmg-ybigta-image.s3.ap-northeast-2.amazonaws.com/Net-{sr}.pt"
+path = "./src/fer_model/ml/trained_models/esr_9/"
+sr = "Net-Base-Shared_Representations"
+url = f"https://kpmg-ybigta-image.s3.ap-northeast-2.amazonaws.com/{sr}.pt"
 r = requests.get(url, allow_redirects=True)
 
-open(f"Net-{sr}.pt", "wb").write(r.content)
-print(f"{sr} saved")
+open(path+f"{sr}.pt", "wb").write(r.content)
+print(path+f"{sr}.pt saved")
 
 for i in range (1, 10):
-    branch = f"Branch_{i}.pt"
-    url = f"https://kpmg-ybigta-image.s3.ap-northeast-2.amazonaws.com/Net-{branch}"
+    branch = f"Net-Branch_{i}"
+    url = f"https://kpmg-ybigta-image.s3.ap-northeast-2.amazonaws.com/{branch}.pt"
     r = requests.get(url, allow_redirects=True)
-    open(f"Net-{branch}", "wb").write(r.content) 
-    print(f"{branch} saved")
+    open(path+f"{branch}.pt", "wb").write(r.content) 
+    print(path+f"{branch}.pt saved")
 
