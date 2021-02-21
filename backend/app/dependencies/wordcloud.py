cde1738863db97f4d15392c9302fa04e644413ca
Sieun <tldms0327@yonsei.ac.kr>
2021-02-21 20:57:13 +0900

    feat: change colors of wordcloud

--- backend/app/dependencies/wordcloud.py
+++ backend/app/dependencies/wordcloud.py
@@ -5,12 +5,23 @@ import string
 import boto3
 
 
-def generate_wordcloud(word_counts):
+def generate_wordcloud(word_counts, TAB):
+    # TAB = "voice"
+    # TAB = "visual"
     IMG_PATH = "img.png"
-    file_name =''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(10)) + '.png'
+
+    if TAB == "text":
+        palette = ["#eb51be", "#fc9be2", "#ffc4dd"]
+    elif TAB == "voice":
+        palette = ["#4238f2", "#608afa", "83b6ff"]
+    elif TAB == "visual":
+        palette = ["#8672f4", "a090fc", "#c6c0ff"]
+
+    file_name = ''.join(
+        random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(10)) + '.png'
     stylecloud.gen_stylecloud(text=word_counts,
                               icon_name="fas fa-cloud",
-                              palette="colorbrewer.diverging.Spectral_11",
+                              colors=palette,
                               output_name=IMG_PATH)
 
     img = Image.open(IMG_PATH)
@@ -29,4 +40,5 @@ def save_into_s3(name):
     img_url = f"https://kpmg-ybigta-image.s3.ap-northeast-2.amazonaws.com/wordcloud/{name}"
 
     return img_url
+
 
